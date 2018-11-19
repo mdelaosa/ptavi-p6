e41b5a6c3af0e93031e8a35137a88646ae062cc8
Maria De La Osa Martinez <mdelaosa@beta16.aulas.gsyc.urjc.es>
2018-11-19 12:03:27 +0100

    Cliente

--- client.py
+++ client.py
@@ -16,16 +16,15 @@ try:
 except (IndexError, ValueError):
     sys.exit('Try: NAME@IP:PORT')
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    if METHOD == 'INVITE':
+        my_socket.send(bytes('INVITE sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
+    if METHOD == 'BYE':
+        my_socket.send(bytes('BYE sip: ' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
     try:
         data = my_socket.recv(1024)
         print('RECEIVED -- ', data.decode('utf-8'))
