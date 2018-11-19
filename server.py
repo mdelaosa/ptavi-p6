29692be5b816d02ff88afcefa2f564bff32d5c85
Maria De La Osa Martinez <mdelaosa@gamma18.aulas.gsyc.urjc.es>
2018-11-19 10:40:29 +0100

    Servidor

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -27,5 +28,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    print("Lanzando servidor SIP de eco...")
+    try:
+        """Creamos el servidor"""
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
