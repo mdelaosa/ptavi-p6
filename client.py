1570338e3cc03037d8a695cc1467e45f3d2435e4
Maria De La Osa Martinez <mdelaosa@alpha47.aulas.gsyc.urjc.es>
2018-11-22 13:35:29 +0100

    Cliente

--- client.py
+++ client.py
@@ -21,14 +21,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    if METHOD == 'INVITE':
-        my_socket.send(bytes('INVITE sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
-    if METHOD == 'BYE':
-        my_socket.send(bytes('BYE sip: ' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
+    if METHOD == 'INVITE' or METHOD == 'BYE':
+        my_socket.send(bytes(METHOD + ' sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
     try:
         data = my_socket.recv(1024)
         print('RECEIVED -- ', data.decode('utf-8'))
         print("FINISHING...")
+        for message in data.decode('utf-8').split(" "):
+            if message == '200 OK' and METHOD != 'BYE':
+                my_socket.send(bytes('ACK sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
     except ConnectionRefusedError:
         print('CONNECTION ERROR')
 
