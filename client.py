2b42bd54b4135191880b7dabde91be26939b1a47
Maria De La Osa Martinez <mdelaosa@alpha47.aulas.gsyc.urjc.es>
2018-11-22 13:38:17 +0100

    Cliente

--- client.py
+++ client.py
@@ -8,10 +8,10 @@ import socket
 import sys
 
 try:
-    METHOD = sys.argv[1] # Método usado.
-    USER = sys.argv[2].split(':')[0] # Receptor+IP.
-    SERVER = USER.split('@')[-1] # IP.
-    PORT = int(sys.argv[2].split(':')[-1]) # Puerto de ejecución.
+    METHOD = sys.argv[1]  # Método usado.
+    USER = sys.argv[2].split(':')[0]  # Receptor+IP.
+    SERVER = USER.split('@')[-1]  # IP.
+    PORT = int(sys.argv[2].split(':')[-1])  # Puerto de ejecución.
 
 except (IndexError, ValueError):
     sys.exit('Try: NAME@IP:PORT')
@@ -22,14 +22,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
 
     if METHOD == 'INVITE' or METHOD == 'BYE':
-        my_socket.send(bytes(METHOD + ' sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
+        my_socket.send(bytes(METHOD + ' sip:' + USER + ' SIP/2.0\r\n',
+                             'utf-8') + b'\r\n')
     try:
         data = my_socket.recv(1024)
         print('RECEIVED -- ', data.decode('utf-8'))
         print("FINISHING...")
         for message in data.decode('utf-8').split(" "):
             if message == '200 OK' and METHOD != 'BYE':
-                my_socket.send(bytes('ACK sip:' + USER + ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
+                my_socket.send(bytes('ACK sip:' + USER + ' SIP/2.0\r\n',
+                                     'utf-8') + b'\r\n')
     except ConnectionRefusedError:
         print('CONNECTION ERROR')
 
