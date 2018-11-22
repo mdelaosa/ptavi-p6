005fe6585fae51da4d4252174f76a232e90a384f
Maria De La Osa Martinez <mdelaosa@alpha47.aulas.gsyc.urjc.es>
2018-11-22 14:18:19 +0100

    Client

--- client.py
+++ client.py
@@ -18,18 +18,17 @@ except (IndexError, ValueError):
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
-
-    if METHOD == 'INVITE' or METHOD == 'BYE':
-        my_socket.send(bytes(METHOD + ' sip:' + USER + ' SIP/2.0\r\n',
-                             'utf-8') + b'\r\n')
     try:
-        data = my_socket.recv(1024)
-        print('RECEIVED -- ', data.decode('utf-8'))
-        print("FINISHING...")
-        for message in data.decode('utf-8').split(" "):
-            if message == '200 OK' and METHOD != 'BYE':
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((SERVER, PORT))
+
+        if METHOD == 'INVITE' or METHOD == 'BYE':
+            my_socket.send(bytes(METHOD + ' sip:' + USER + ' SIP/2.0\r\n',
+                                 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+            print('RECEIVED -- ', data.decode('utf-8'))
+            print("FINISHING...")
+            if data.decode('utf-8').split(" ")[2] == '200':
                 my_socket.send(bytes('ACK sip:' + USER + ' SIP/2.0\r\n',
                                      'utf-8') + b'\r\n')
     except ConnectionRefusedError:
