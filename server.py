65de213b93743a2de65d8a8409cee17cd3360ff1
Maria De La Osa Martinez <mdelaosa@alpha47.aulas.gsyc.urjc.es>
2018-11-22 13:52:45 +0100

    Servidor

--- server.py
+++ server.py
@@ -20,20 +20,22 @@ class SIPHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            if line.decode('utf-8').split(' ')[0] == 'INVITE':
+            METHOD = line.decode('utf-8').split(' ')
+            if METHOD == 'INVITE':
                 print("THE CLIENT SENT: " + line.decode('utf-8'))
                 self.wfile.write(b"SIP/2.0 100 TRYING...\r\n\r\n" +
                                  b"SIP/2.0 100 RINGING...\r\n\r\n" +
                                  b"SIP/2.0 200 OK...\r\n\r\n")
-            if line.decode('utf-8').split(' ')[0] == 'ACK':
+            if METHOD == 'ACK':
                 aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
                 print('Vamos a ejecutar', aEjecutar)
                 os.system(aEjecutar)
                 self.wfile.write(b"cancion.mp3 enviada \r\n\r\n")
-            if line.decode('utf-8').split(' ')[0] == 'BYE':
+            if METHOD == 'BYE':
                 print('FINISHING CONNECTION WITH THE CLIENT')
-            if
-            else:
+            #if self.error(line.decode('utf-8').split(' ')):
+                #self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+            if METHOD != 'INVITE' or 'ACK' or 'BYE':
                 self.wfile.write(b"SIP/2.0 405 METHOD NOT ALLOWED\r\n\r\n")
             # Si no hay más líneas salimos del bucle infinito
             if not line:
