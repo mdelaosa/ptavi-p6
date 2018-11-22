7bf7ae3d57b8f0f40c229fda09dffd8d62252c97
Maria De La Osa Martinez <mdelaosa@alpha47.aulas.gsyc.urjc.es>
2018-11-22 13:35:40 +0100

    Servidor

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 import os
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
@@ -24,15 +25,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 self.wfile.write(b"SIP/2.0 100 TRYING...\r\n\r\n" +
                                  b"SIP/2.0 100 RINGING...\r\n\r\n" +
                                  b"SIP/2.0 200 OK...\r\n\r\n")
+            if line.decode('utf-8').split(' ')[0] == 'ACK':
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
+                print('Vamos a ejecutar', aEjecutar)
+                os.system(aEjecutar)
+                self.wfile.write(b"cancion.mp3 enviada \r\n\r\n")
             if line.decode('utf-8').split(' ')[0] == 'BYE':
                 print('FINISHING CONNECTION WITH THE CLIENT')
+            if
+            else:
+                self.wfile.write(b"SIP/2.0 405 METHOD NOT ALLOWED\r\n\r\n")
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    PORT = int(sys.argv[2])
+    serv = socketserver.UDPServer(('', PORT), SIPHandler)
     print("Lanzando servidor SIP de eco...")
     try:
         """Creamos el servidor"""
