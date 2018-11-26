d192ff3f3e0928d23ef818834aa7022090310c4d
Maria De La Osa Martinez <mdelaosa@gamma19.aulas.gsyc.urjc.es>
2018-11-26 10:25:56 +0100

    Servidor

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de SIP simple.
-"""
+"""Clase (y programa principal) para un servidor de SIP simple."""
 
 import socketserver
 import sys
@@ -10,20 +8,17 @@ import os
 
 
 class SIPHandler(socketserver.DatagramRequestHandler):
-    """
-    SIP server class.
-    """
+    """SIP server class."""
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address).
+        """Escribe dirección y puerto del cliente (de tupla client_address)."""
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente.
             line = self.rfile.read()
             if not line:
                 break
-            linea = line.decode('utf-8')
-            (method, address, sip) = linea.split()
-            print("THE CLIENT SENT: " + linea)
+            method = line.decode('utf-8').split(" ")[0]
+            print("THE CLIENT SENT: " + line.decode('utf-8'))
             if method == 'INVITE':
                 self.wfile.write(b"SIP/2.0 100 TRYING...\r\n\r\n" +
                                  b"SIP/2.0 100 RINGING...\r\n\r\n" +
@@ -33,17 +28,16 @@ class SIPHandler(socketserver.DatagramRequestHandler):
                 song = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
                 print('SONG', song)
                 os.system(song)
-                self.wfile.write(b"cancion.mp3 SENT \r\n\r\n")
-                print('hola')
                 break
             if method == 'BYE':
+                self.wfile.write(b"SIP/2.0 200 OK FINISHIN CONNECTION")
                 print('FINISHING CONNECTION WITH THE CLIENT')
                 break
-            if len(line.decode('utf-8').split(" ")) != 3:
-                self.wfile.write(b"SIP/2.0 400 BAD REQUEST\r\n\r\n")
+            if method != ('INVITE', 'ACK', 'BYE'):
+                self.wfile.write(b"SIP/2.0 405 METHOD NOT ALLOWED\r\n\r\n")
                 break
             else:
-                self.wfile.write(b"SIP/2.0 405 METHOD NOT ALLOWED\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 400 BAD REQUEST\r\n\r\n")
                 break
 
 
